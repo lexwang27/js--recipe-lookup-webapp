3deed6327ae2c917e805e7912bed1c781d5e7aef
Lex Wang <ziwang.us@gmail.com>
2018-04-13 13:12:11 +0800

    First Commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'd2d4fd5061c8ee8c16e5835ed9672a23';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+        
+    }
+}
+
+
+
